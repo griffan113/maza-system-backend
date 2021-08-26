794c43c48f193b127f03c8a8acdc4face829e057
Eliel M. Gaspar <elielmichel@gmail.com>
2021-08-26 14:58:34 -0300

    Created NestJs project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { AppModule } from './shared/infra/app.module';
+
+async function bootstrap() {
+  const logger = new Logger('bootstrap');
+  const port = process.env.PORT;
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+
+  logger.log('Application listening on port: ' + port);
+}
+bootstrap();
---
+++ src/shared/infra/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
+})
+export class AppModule {}
