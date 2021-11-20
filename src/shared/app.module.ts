90932d471e568ef805419079de9a5480841507c3
Eliel <elielmichel@gmail.com>
2021-11-20 11:24:25 -0300

    build: remove TypeORM

--- src/shared/app.module.ts
+++ src/shared/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import OrdersModule from '@modules/orders/orders.module';
 import Container from '@shared/container/container.module';
@@ -10,7 +9,6 @@ import { PrismaService } from '@shared/services/Prisma.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
